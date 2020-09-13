97a05b0c2b4043edf9e9cac84b9e4a305e12d769
bech-21 <bachir.mahomad@gmail.com>
2020-09-12 22:52:29 -0600

    updating class

---
+++ class/class3.py
@@ -0,0 +1,24 @@
+class Flight:
+    def __init__(self,origin,destination,duration):
+        self.origin=origin
+        self.destination=destination
+        self.duration=duration
+
+    # print function
+    def print_info(self):
+      print(f"FLight Ori: {self.origin}")
+      print(f"FLight des: {self.destination}")
+      print(f"FLight duration: {self.duration}")
+
+    def delay(self, amount):
+        self.duration+=amount
+      
+
+def main():
+    f1 =Flight(origin="New York", destination="Paris",duration=540)
+    f1.delay(10)
+    f1.print_info()
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ class/class4.py
@@ -0,0 +1,65 @@
+class Flight:
+    counter=1
+    def __init__(self,origin,destination,duration):
+        # keep track of id number 
+        self.id=Flight.counter
+        Flight.counter+=1
+        
+        # Keep track of passenger 
+        self.passengers =[]
+
+
+        #  Details of flights 
+        self.origin=origin
+        self.destination=destination
+        self.duration=duration
+
+    # print function
+    def print_info(self):
+      print(f"FLight Ori: {self.origin}")
+      print(f"FLight des: {self.destination}")
+      print(f"FLight duration: {self.duration}")
+
+      print()
+
+      print("passengers:")
+
+      for passenger in self.passengers:
+          print(f"Passenger name: {passenger.name}")
+
+    # Method used to reduce the duration of flight by certain amount,
+    def delay(self, amount):
+        self.duration+=amount
+
+     # Method used to tight together the flight and the passenger ,
+    def add_passenger(self,p):
+        self.passengers.append(p)
+        p.flight_id=self.id
+      
+      
+class passenger:
+
+    def __init__(self,name):
+        # name  of passenger
+        self.name=name
+
+
+
+def main():
+    # creating a flight object 
+    f1 =Flight(origin="New York", destination="Paris",duration=540)
+
+    #creating a passenger object 
+    alice=passenger(name="Alice")
+    bob=passenger(name="Bob")
+
+    #adding the passenger object to the flight object using the method add_passeger method of the flights object
+    f1.add_passenger(alice)
+    f1.add_passenger(bob)
+
+    # printing the result 
+    f1.print_info()
+    
+    
+if __name__ == "__main__":
+    main()
